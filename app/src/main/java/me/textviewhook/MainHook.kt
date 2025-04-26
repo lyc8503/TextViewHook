a4e6b84c16abf599532db2047623104334069461
lyc8503 <me@lyc8503.net>
2025-04-26 23:43:45 +0800

    looks good, TODO test

--- app/src/main/java/me/textviewhook/MainHook.kt
+++ app/src/main/java/me/textviewhook/MainHook.kt
@@ -8,58 +8,84 @@ import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import org.w3c.dom.Text
 
 class MainHook : IXposedHookLoadPackage {
-    fun doReplace(charSeq: CharSequence): CharSequence {
-        return charSeq.replace(Regex("lyc8503"), "lyc8504")
+    fun doSpannedReplace(original: Spanned, oldStr: String, newStr: String): SpannableStringBuilder {
+        data class SpanInfo(
+            val span: Any,
+            var start: Int,
+            var end: Int,
+            val flags: Int
+        )
+
+        val spanList = mutableListOf<SpanInfo>()
+        original.getSpans(0, original.length, Any::class.java).forEach {
+            val start = original.getSpanStart(it)
+            val end = original.getSpanEnd(it)
+            val flags = original.getSpanFlags(it)
+            spanList.add(SpanInfo(it, start, end, flags))
+            XposedBridge.log("SpanInfo: $it $start $end $flags Class: ${it.javaClass.name}")
+        }
+
+        var modifiedText = original.toString()
+        outer@ for (current in original.indices) {
+            // Match text to be replaced
+            XposedBridge.log("DEBUG current $current")
+            if (current + oldStr.length <= original.length && original.subSequence(current, current + oldStr.length).toString() == oldStr) {
+                XposedBridge.log("DEBUG found $oldStr at $current")
+                val strStart = current
+                val strEnd = current + oldStr.length
+
+                // See if the replaced text has different spans, doesn't replace if it does
+                for (spanInfo in spanList) {
+                    if (spanInfo.start in (strStart + 1)..<strEnd) {
+                        XposedBridge.log("DEBUG continued due to $spanInfo start")
+                        continue@outer
+                    }
+                    if (spanInfo.end in (strStart + 1)..<strEnd) {
+                        XposedBridge.log("DEBUG continued due to $spanInfo end")
+                        continue@outer
+                    }
+                }
+
+                modifiedText = original.subSequence(0, current).toString() + newStr + original.subSequence(strEnd, original.length)
+                // Move spans to the new text
+                for (spanInfo in spanList) {
+                    spanInfo.start = if (spanInfo.start < strEnd) spanInfo.start else spanInfo.start + newStr.length - oldStr.length
+                    spanInfo.end = if (spanInfo.end <= strStart) spanInfo.end else spanInfo.end + newStr.length - oldStr.length
+                }
+            }
+        }
+
+        val modified = SpannableStringBuilder(modifiedText)
+        spanList.forEach {
+            modified.setSpan(it.span, it.start, it.end, it.flags)
+        }
+        return modified
     }
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        // Don't hook itself
         if (lpparam.packageName == "me.textviewhook") return
 
         val methodHook = object : XC_MethodHook() {
             override fun beforeHookedMethod(param: MethodHookParam) {
+                // Change the text to be displayed in TextView (while keeping the spans)
                 if (param.args[0] == null) return
 
                 XposedBridge.log("TextView setText arg: ${param.args[0]} Class: ${param.args[0].javaClass.name}")
 
+//                param.args[0] = (param.thisObject as TextView).id.toString()
+
                 if (param.args[0] is Spanned) {
-                    val originalSpanned = param.args[0] as Spanned
-                    val modifiedSpanned = SpannableStringBuilder()
-
-                    var current = 0
-                    originalSpanned.getSpans(0, originalSpanned.length, Any::class.java).forEach { span ->
-                        val start = originalSpanned.getSpanStart(span)
-                        val end = originalSpanned.getSpanEnd(span)
-                        val flags = originalSpanned.getSpanFlags(span)
-                        XposedBridge.log("Span: $span $start $end $flags Class: ${span.javaClass.name}")
-
-                        if (start > current) {
-                            // Copy the gap between the last span and the current span
-                            val gap = originalSpanned.subSequence(current, start)
-                            modifiedSpanned.append(doReplace(gap))
-                        } else {
-                            val newText = doReplace(originalSpanned.subSequence(start, end))
-                            XposedBridge.log("New Text: $newText")
-                            modifiedSpanned.append(newText, span, flags)
-                        }
-                        current = start
-                    }
-
-                    // Copy the remaining text after the last span (also handles the case where there are no spans)
-                    if (current < originalSpanned.length) {
-                        val remainingText = originalSpanned.subSequence(current, originalSpanned.length)
-                        modifiedSpanned.append(doReplace(remainingText))
-                    }
-
-                    param.args[0] = modifiedSpanned
+                    if ((param.args[0] as Spanned).length != 8) return
+                    param.args[0] = doSpannedReplace(param.args[0] as Spanned, "lyc8503", "lyc8504")
                     return
                 }
-                // Modify the text before it is set
+
                 val originalText = param.args[0] as CharSequence
-                param.args[0] = doReplace(originalText)
-//                val modifiedText = "Hooked: $originalText"
-//                param.args[0] = modifiedText
+                param.args[0] = originalText.replace(Regex("lyc8503"), "lyc8504")
             }
         }
 
